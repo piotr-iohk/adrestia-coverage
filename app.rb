576ab23237ca12dead09ce39f68362ec2c2f3585
Piotr Stachyra <piotr.stachyra@iohk.io>
2020-04-16 17:23:04 +0200

    Add average coverage

--- app.rb
+++ app.rb
@@ -9,8 +9,14 @@ class App < Sinatra::Base
   set :root, File.dirname(__FILE__)
   helpers Helpers
 
-  url_cardano_transactions = "https://input-output-hk.github.io/cardano-transactions/coverage/"
-  url_cardano_coin_selection = "https://input-output-hk.github.io/cardano-coin-selection/coverage/"
+  url_cardano_transactions = "https://input-output-hk.github.io/cardano-transactions/coverage/hpc_index.html"
+  url_cardano_coin_selection = "https://input-output-hk.github.io/cardano-coin-selection/coverage"
+
+  def generic(url, which)
+    html = URI.open(url).read
+    cov = get_coverage html, which
+    prepare_response(cov)
+  end
 
   get "/" do
     erb :index
@@ -18,22 +24,20 @@ class App < Sinatra::Base
 
   # /cardano-transactions
   get "/cardano-transactions" do
-    redirect "/cardano-transactions/top-level-definitions"
+    generic url_cardano_transactions, "avg"
   end
 
   get "/cardano-transactions/:which" do
-    cov = get_coverage url_cardano_transactions, params[:which]
-    prepare_response(cov)
+    generic url_cardano_transactions, params[:which]
   end
 
   # /cardano-coin-selection
   get "/cardano-coin-selection" do
-    redirect "/cardano-coin-selection/top-level-definitions"
+    generic url_cardano_coin_selection, "avg"
   end
 
   get "/cardano-coin-selection/:which" do
-    cov = get_coverage url_cardano_coin_selection, params[:which]
-    prepare_response(cov)
+    generic url_cardano_coin_selection, params[:which]
   end
 
 end
--- helpers.rb
+++ helpers.rb
@@ -13,18 +13,27 @@ module Helpers
     "//th[contains(text(),'Program Coverage Total')]/../td[#{i}]"
   end
 
-  def get_coverage(url, which)
-    html = URI.open(url).read
+  def get_coverage(html, which)
     doc = Nokogiri::HTML(html)
-    xpath = get_xpath(which)
+    case which
+    when 'avg'
+      x1, x2, x3 = get_xpath('top-level-definitions'), get_xpath('alternatives'), get_xpath('expressions')
+      c1, c2, c3 = doc.xpath(x1).to_s, doc.xpath(x2).to_s, doc.xpath(x3).to_s
+      covers = [/[0-9]{1,3}/.match(c1),
+                /[0-9]{1,3}/.match(c2),
+                /[0-9]{1,3}/.match(c3)].map{|i| i.to_s.to_f}
+      cov = (covers.sum / 3).to_i
+    else
+      xpath = get_xpath(which)
+      cov_html = doc.xpath(xpath).to_s
+      cov = /[0-9]{1,3}/.match(cov_html).to_s.to_i
+    end
 
-    cov_html = doc.xpath(xpath).to_s
-    cov = /[0-9]{1,3}/.match(cov_html)
     cov
   end
 
   def prepare_response(cov)
-    case cov.to_s.to_i
+    case cov
     when 0..30
       color = "red"
     when 30..50
@@ -38,6 +47,7 @@ module Helpers
     when 90..100
       color = "brightgreen"
     end
+
     content_type :json
     {
       schemaVersion: 1,
@@ -48,3 +58,10 @@ module Helpers
   end
 
 end
+
+include Helpers
+require 'open-uri'
+require 'nokogiri'
+html = URI.open("https://input-output-hk.github.io/cardano-transactions/coverage/hpc_index.html").read
+cov = get_coverage html, 'avg'
+p cov
